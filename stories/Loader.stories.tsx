f8b61209c4f3ef78c4a4b4919b2939b0233d41a8
Shelton Suen <sheltonsuen@gmail.com>
2019-09-06 20:30:14 +0800

    #24 - rebuild project using hexo with cactus themes.

--- stories/Loader.stories.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-import Loader from '../src/components/Loader';
-
-storiesOf('Loader', module)
-    .add('default', () => (<Loader content='Loading...' />));
