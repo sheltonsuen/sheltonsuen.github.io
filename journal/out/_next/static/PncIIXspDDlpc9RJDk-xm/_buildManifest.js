abc0f5e37181d689818a0f2cd085c5018cfb1d78
Shelton Suen <sheltonsuen@gmail.com>
2022-02-25 07:30:20 +0000

    Publish a new version

---
+++ journal/out/_next/static/PncIIXspDDlpc9RJDk-xm/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/149b18973e5508c7.css","static/chunks/pages/index-533a2f8c19a46da7.js"],"/_error":["static/chunks/pages/_error-faac9cd9578e199c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
